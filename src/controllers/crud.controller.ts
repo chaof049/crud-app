d12ab6c56bf68fdac56d4c9b704449f4f1049bae
supremepradhananga <chaof049@gmail.com>
2023-08-13 12:24:38 +0545

    just checking

--- src/controllers/crud.controller.ts
+++ src/controllers/crud.controller.ts
@@ -14,6 +14,7 @@ import {Crud} from '../models/crud.model';
 import {CrudRepository} from '../repositories/crud.repository';
 
 
+
 @authenticate('jwt')
 export class CrudController {
   constructor(
